44dbb3c6fc04c5554f85b974143b964891ec689c
Evan Pratten <ewpratten@gmail.com>
2017-02-21 11:10:18 -0500

    v 1.0

---
+++ pyp2.py
@@ -0,0 +1,36 @@
+import os
+
+#change version number here
+vers = 1.0
+
+
+print("pyp2")
+print(vers)
+
+print()
+
+print("Options: install, search, remove, update")
+sel1 = input(">")
+
+print()
+
+if sel1 == 'install' ;
+	
+
+else
+	if sel1 == 'search' ;
+		
+
+	else
+		if sel1 == 'remove' ;
+		print("sorry, this function is still in development")
+		print("mabey it will be fixed in the next update?")
+		print()
+		
+			
+
+		else
+			if sel1 == 'update' ;
+				
+
+			else
